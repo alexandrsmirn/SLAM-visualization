ca4f3eec3f581d5d7f5f5bef914a65f5d4a9c806
Grismat <smirnalexandr@rambler.ru>
2020-11-04 23:14:21 +0300

    Changed file structure and added simple GUI test

---
+++ GUI_test.py
@@ -0,0 +1,56 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+from gaphas import Canvas, GtkView
+from gaphas.examples import Box
+from gaphas.painter import DefaultPainter
+from gaphas.item import Line
+from gaphas.segment import Segment
+
+from random import randint
+
+def add_box(canvas):
+    b2 = Box(60, 60)
+    b2.min_width = 40
+    b2.min_height = 50
+    b2.matrix.translate(randint(50, 350), randint(50, 350))
+    canvas.add(b2)
+
+def add_line(canvas):
+    line = Line()
+    line.matrix.translate(randint(50, 350), randint(50, 350))
+    view.canvas.add(line)
+    line.handles()[1].pos = (30, 30)
+    #segment = Segment(line, canvas)
+    #segment.split_segment(0)
+
+class Handler:
+    def __init__(self, canvas):
+        self.canvas = canvas
+
+    def on_MainWindow_destroy(self, *args):
+        Gtk.main_quit()
+
+    def on_btn1_clicked(self, button):
+        add_box(self.canvas)
+
+    def on_btn2_clicked(self, button):
+        add_line(self.canvas)
+
+builder = Gtk.Builder()
+builder.add_from_file("GUI_test.glade")
+
+view = GtkView() #Gtk widget
+view.painter = DefaultPainter()
+view.canvas = Canvas()
+
+builder.connect_signals(Handler(view.canvas))
+
+gaphas_window = builder.get_object("GaphasWindow")
+gaphas_window.add(view)
+
+window = builder.get_object("MainWindow")
+window.show_all()
+Gtk.main()
